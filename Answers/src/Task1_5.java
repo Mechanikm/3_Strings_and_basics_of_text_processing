7c435ba8aa26dc887390dcd9ee9c2ff87d0fe227
Mechanikm <mechanik@email.com>
2022-06-13 09:50:47 +0300

    changed method for Task 1.5

--- Answers/src/Task1_5.java
+++ Answers/src/Task1_5.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 // Удалить в строке все лишние пробелы, 
@@ -11,15 +12,23 @@ public class Task1_5 {
         try (Scanner scan = new Scanner(System.in)) {
             System.out.print("Enter string with extra spaces: ");
             String stringWithSpaces = scan.nextLine();
-            stringWithSpaces = stringWithSpaces.trim();
+            String[] arrayWithSpaces = stringWithSpaces.trim().split(" ");
 
-            while (stringWithSpaces.contains("  ")) {
-                String replace = stringWithSpaces.replace("  ", " ");
-                stringWithSpaces = replace;
+            String stringWithoutExtraSpaces;
+            ArrayList<String> arrayWithoutExtraSpaces = new ArrayList<String>();
+
+            arrayWithoutExtraSpaces.add(arrayWithSpaces[0]);
+
+            for (int i = 1; i < arrayWithSpaces.length; i++) {
+                if (arrayWithSpaces[i] != " " && arrayWithSpaces[i] != "") {
+                    arrayWithoutExtraSpaces.add(" ");
+                    arrayWithoutExtraSpaces.add(arrayWithSpaces[i]);
+                }
             }
+            stringWithoutExtraSpaces = String.join("", arrayWithoutExtraSpaces);
 
             System.out.println("There is a string without extra spaces:");
-            System.out.print(stringWithSpaces);
+            System.out.println(stringWithoutExtraSpaces);
         }
     }
 }
